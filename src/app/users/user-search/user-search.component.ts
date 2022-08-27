da38e4ce512fd45d936f049b95a9783cda071414
Dave <88671265+shagat@users.noreply.github.com>
2022-08-27 22:53:56 +0530

    added search query property

--- src/app/users/user-search/user-search.component.ts
+++ src/app/users/user-search/user-search.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-search',
   templateUrl: './user-search.component.html',
-  styleUrls: ['./user-search.component.css']
+  styleUrls: ['./user-search.component.css'],
 })
 export class UserSearchComponent implements OnInit {
+  searchQuery: string = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
